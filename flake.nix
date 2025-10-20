b5304e07ba4a3861a099f997924d50f6ddae1e6d
lipeeeee <filipe.pereirafga@gmail.com>
2025-10-20 23:35:38 +0100

    initial

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "Nixos conf";
+
+  inputs = {
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  let
+    # Reusable function to configure a single NixOS system
+    mkNixosSystem = { system, hostname, username, extraModules ? [] }:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          ./hosts/${hostname}/configuration.nix
+          
+          # 2. Integrate Home Manager as a NixOS module
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              
+              # Specify which user's config to apply on this host
+              users.${username} = import ./hosts/${hostname}/home.nix;
+            };
+          }
+        ] ++ extraModules;
+      };
+
+  in {
+    nixosConfigurations = {
+      desktop = mkNixosSystem {
+        system = "x86_64-linux";
+        hostname = "desktop";
+        username = "lipe";
+      };
+
+      laptop = mkNixosSystem {
+        system = "x86_64-linux";
+        hostname = "laptop";
+        username = "lipe"; 
+      };
+    };
+  };
+}
+
+# {
+#   description = "Nixos config flake";
+#
+#   inputs = {
+#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+#
+#     home-manager = {
+#       url = "github:nix-community/home-manager";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#     };
+#   };
+#
+#   outputs = { self, nixpkgs, ... }@inputs: {
+#     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+#       specialArgs = {inherit inputs;};
+#       modules = [
+#         ./hosts/${hostname}/configuration.nix
+#         inputs.home-manager.nixosModules.default
+#       ];
+#     };
+#   };
+# }
