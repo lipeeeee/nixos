e37587abecfe817e574abe2ebb31dba044e609ca
lipeeeee <filipe.pereirafga@gmail.com>
2025-10-21 23:02:12 +0100

    vim stuff

--- modules/editors/vim/vim.nix
+++ modules/editors/vim/vim.nix
@@ -1,11 +1,19 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [
-    vim 
-  ];
-
   programs.vim = {
     enable = true;
+  };
+
+  home.file.".vimrc" = {
+    # Destination: ~/.vimrc
+    target = ".vimrc"; 
+    
+    source = ./.vimrc; 
+    
+    # Tell Home Manager to create a symlink (default is copy, but symlink is common)
+    # The default is generally sufficient, but you can be explicit:
+    # recursive = false;
+    # executable = false;
   };
 }
