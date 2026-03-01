e7a7401704843095624ddfcb4b7aa0aa8e632767
lipeeeee <filipe.pereirafga@gmail.com>
2026-03-01 21:28:29 +0000

    unsuefull comment for hypridle

--- modules/system/hypridle.nix
+++ modules/system/hypridle.nix
@@ -9,9 +9,9 @@
     enable = true;
     settings = {
       general = {
-        lock_cmd = "pidof hyprlock || hyprlock"; # Avoid starting multiple instances
+        lock_cmd = "pidof hyprlock || hyprlock";
         before_sleep_cmd = "loginctl lock-session"; # Lock before suspend
-        after_sleep_cmd = "hyprctl dispatch dpms on"; # Turn screen on when waking
+        after_sleep_cmd = "hyprctl dispatch dpms on";
       };
   
       listener = [
